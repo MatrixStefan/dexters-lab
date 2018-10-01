2179b66a0225ea0160ed37449ce6fd35a70f63f1
MatrixStefan <stefan.ritchie@thisismatrix.com>
2018-10-01 10:49:10 +0100

    Root switch

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :chin_wags
-  root to: 'chin_wags#index'
+  root to: 'file_uploads#index'
 
   resources :file_uploads
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
